ae3b1039f357104da4a01c4e41482b65bca534f7
seed4407 <sebastian.castrod@usm.cl>
2023-09-30 23:57:48 -0300

    todo funcional a excepcion de guardado de ultima id creada

--- app/main.py
+++ app/main.py
@@ -8,46 +8,48 @@ from pydantic import BaseModel
 import requests
 import time
 import pika
+import signal
 
 def comunicacion_publicidad(background_tasks, interval):
     contador_anuncios = 0
-    while True:
-        try:
-            response = requests.get('http://172.18.0.5:80/')
-            response.raise_for_status()  # Verificar el código de estado HTTP
-            # Procesar la respuesta exitosa
-            data = response.json()
+    try:
+        while True:
+            try:
+                response = requests.get('http://172.18.0.5:80/')
+                response.raise_for_status()  # Verificar el código de estado HTTP
+                # Procesar la respuesta exitosa
+                data = response.json()
 
-        except requests.exceptions.RequestException as e:
-            logging.info(f"Error de conexión: {e}")
-        except requests.exceptions.HTTPError as e:
-            logging.info(f"Error HTTP: {e}")
-        except requests.exceptions.Timeout as e:
-            logging.info(f"Tiempo de espera agotado: {e}")
+            except requests.exceptions.RequestException as e:
+                logging.info(f"Error de conexión: {e}")
+            except requests.exceptions.HTTPError as e:
+                logging.info(f"Error HTTP: {e}")
+            except requests.exceptions.Timeout as e:
+                logging.info(f"Tiempo de espera agotado: {e}")
 
-        estado = data[0]["estado"]
-        if(estado == "conectado"):
-            anuncios_para_enviar = mongodb_client.service_01.anuncios.find_one({ "id": { "$gte": str(contador_anuncios) } })
-            if(anuncios_para_enviar == None):
-                contador_anuncios = 0
+            estado = data[0]["estado"]
+            if(estado == "conectado"):
                 anuncios_para_enviar = mongodb_client.service_01.anuncios.find_one({ "id": { "$gte": str(contador_anuncios) } })
-                contador_anuncios = int(anuncios_para_enviar["id"]) + 1
-            else:
-                contador_anuncios = int(anuncios_para_enviar["id"]) + 1
-            
-            connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.18.0.6'))
-            channel = connection.channel()
-        # channel.queue_declare(queue='hello')
-            channel.basic_publish(exchange='', routing_key='publicidad', body=anuncios_para_enviar["description"])
+                if(anuncios_para_enviar == None):
+                    contador_anuncios = 0
+                    anuncios_para_enviar = mongodb_client.service_01.anuncios.find_one({ "id": { "$gte": str(contador_anuncios) } })
+                    contador_anuncios = int(anuncios_para_enviar["id"]) + 1
+                else:
+                    contador_anuncios = int(anuncios_para_enviar["id"]) + 1
+                connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.18.0.6'))
+                channel = connection.channel()
+                # channel.queue_declare(queue='hello')
+                channel.basic_publish(exchange='', routing_key='publicidad', body=anuncios_para_enviar["description"])
             connection.close()
         time.sleep(interval)
+    except KeyboardInterrupt:
+        logging.info("¡Ctrl + C detectado! Terminando el programa.")
 
-app = FastAPI()
-mongodb_client = MongoClient("demo_01_service_01_mongodb", 27017)
-
-logging.basicConfig(level = logging.DEBUG,
-                    format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s',
-                    encoding='utf-8')
+def actualizacion_ultimo_id():
+    global id_anuncios
+    logging.info("dssadsd")
+    logging.info(id_anuncios)
+    mongodb_client.service_01.cantidades.update_one({},{"$set": {"cantidad":id_anuncios}})
 
 class publicidad(BaseModel):
     id: str | None = None
@@ -57,14 +59,30 @@ class publicidad(BaseModel):
 class cantidad(BaseModel):
     cantidad: int
 
-if(mongodb_client.service_01.cantidad.find() == None):
-    valor_inicial = cantidad(1)
-    inserted_id = mongodb_client.service_01.cantidades.insert_one(
+signal.signal(signal.SIGINT, actualizacion_ultimo_id)
+
+app = FastAPI()
+mongodb_client = MongoClient("demo_01_service_01_mongodb", 27017)
+
+logging.basicConfig(level = logging.DEBUG,
+                    format = '%(asctime)s:%(levelname)s:%(name)s:%(message)s',
+                    encoding='utf-8')
+
+if(mongodb_client.service_01.cantidades.find_one() == None):
+    valor_inicial = cantidad(cantidad=1)
+    # logging.info(valor_inicial)
+    resultado = mongodb_client.service_01.cantidades.insert_one(
         valor_inicial.dict()
-    ).inserted_id
+    )
     id_anuncios = 1
 else:
-    id_anuncios = mongodb_client.service_01.cantidades.find_one()["cantidad"]
+    dato = mongodb_client.service_01.cantidades.find_one()
+    # logging.info(dato)
+    if(dato == None):
+        id_anuncios = 1
+    else:
+        valor = dato
+        id_anuncios = int(valor["cantidad"])
 
 @app.get("/")
 async def root(background_tasks: BackgroundTasks, interval = 1):
@@ -77,12 +95,11 @@ def creacion_anuncio(anuncio: publicidad):
     """
     Creacion de anuncion que se enviaran:
 
-    - **id**: identificador del anuncio
     - **name**: titulo para identificar a quien pertenece el anuncio
     - **description**: contenido del anuncio
     """
     global id_anuncios
-    anuncio.id = id_anuncios
+    anuncio.id = str(id_anuncios)
     id_anuncios = id_anuncios + 1
     inserted_id = mongodb_client.service_01.anuncios.insert_one(
         anuncio.dict()
@@ -100,13 +117,11 @@ def creacion_anuncio(anuncio: publicidad):
 
 @app.get("/anuncio/{anuncio_id}")
 def anuncio_get(anuncio_id: str):
-    # logging.info(mongodb_client.service_01.anuncios.estimated_document_count())
-    logging.info((mongodb_client.service_01.anuncios.find_one())["id"])
     dato = mongodb_client.service_01.anuncios.find_one({"id": anuncio_id})
     if(dato == None):
         return None
     else:
-        return publicidad(dato)
+        return publicidad(**dato)
 
 @app.get("/anuncio_all")
 def anuncio_all():
@@ -114,7 +129,7 @@ def anuncio_all():
 
 @app.delete("/anuncios_eliminar/{anuncio_id}")
 def anuncios_eliminar(anuncio_id: str):
-    mongodb_client.service_01.anuncios.delete_many(
+    result = mongodb_client.service_01.anuncios.delete_one(
         {"id": anuncio_id}
     )
-    return "ok"
+    return result.deleted_count
